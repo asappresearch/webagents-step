16a850bace9b9b49d06e9fedfbeebec4dbc0a02a
Paloma Sodhi <psodhi@asapp.com>
2024-06-10 20:36:57 -0400

    prompt improvements

--- src/webagents_step/prompts/webarena/step_fewshot_template.py
+++ src/webagents_step/prompts/webarena/step_fewshot_template.py
@@ -490,7 +490,7 @@ Completion Action:
 `stop [answer]`: Issue this action when you believe the task is complete. If the objective is to find a text-based answer, provide the answer in the bracket. If you believe the task is impossible to complete, provide the answer as "N/A" in the bracket.
 
 Subroutine Actions:
-`find_subreddit [query]`: Find a subreddit corresponding to the query. The query can either be the name of the subreddit or a vague description of what the subreddit may contain. The subroutine hands back control once it navigates to the subreddit. 
+`find_subreddit [query]`: Find a subreddit corresponding to the query. The query can either be the name of the subreddit or a informative description of what the subreddit may contain. The subroutine hands back control once it navigates to the subreddit by returning "N/A" to denote success.
 `find_user [user_name]`: Navigate to the page of a user with user_name. The page contains all the posts made by the user.
 
 Example actions:
@@ -499,6 +499,7 @@ type [15] [Carnegie Mellon University] [1]
 stop [Closed]
 find_subreddit [books]
 find_subreddit [something related to driving in Pittsburgh]
+find_subreddit [most appropriate subreddit for X]
 find_user [AdamCannon]
 
 You will be provided with the following,
@@ -519,7 +520,10 @@ You need to generate a response in the following format. Please issue only a sin
  
 Please follow these general instructions:
 1. If you have to do a task related to a particular user, first find the user using find_user subroutine
-2. Otherwise, if you have to post or edit a post in a subreddit, first find the subreddit using the find_subreddit subroutine
+2. Otherwise, if you have to post or edit a post in a subreddit, first find the subreddit using the find_subreddit subroutine. Pass in as much information in the argument. While find_subreddit will return the most relevant subreddit to your query, it is okay if it does not exactly match your query. 
+3. When making a post or a comment to a reply, look at your OBSERVATION or PREVIOUS ACTIONS to make sure you are not repeating the same action.
+4. When typing the "Title" of a submission, make sure to match the phrasing in objective exactly. If the objective said Post "what could X", type that in exactly as the title. In your REASON, you MUST specify the formatting guidelines you are following.
+5. When creating a Forum, be sure to fill in the title, description and sidebar as specified in the objective exactly. 
 """,
 
 "input": """
@@ -573,11 +577,13 @@ ACTION:
 Your action
 
 Please follow these instructions to solve the subtask:
-1. The objective find_subreddit [query] asks you to navigate to the subreddit that best matches the query. The query can be specific or vague. 
-2. To navigate to a subreddit, first click on Forums from the top menu.
-3. Once you are in the Forums page, and you see the Alphabetical option, click on it to see a list of all subreddits alphabetically.
-4. Once you are in the page with all the subreddits listed alphabetically, click on the subreddit that matches the query
-5. Once you have navigated to the subreddit, return stop [N/A]. You can check that you are in the subreddit by looking at the current observation and seeing "heading '/f/subreddit_name'". You will also see a number of posts. If the subreddit_name vaguely matches the query, it means you are already in the subreddit and should stop, e.g. gaming and games are the same subreddit.
+* The objective find_subreddit [query] asks you to navigate to the subreddit that best matches the query. The query can be specific or vague. 
+* The first step is to navigate to Forums to see the list of subreddits. However, if you have done this already (indicated as non empty PREVIOUS ACTIONS), do not repeat this step.
+* Under forums, you will see only a subset of subreddits. To get the full list of subreddits, you need to navigate to the Alphabetical option.
+* To know you can see the full list of subreddits, you will see 'All Forums' in the observation
+* Often you will not find a focused subreddit that exactly matches your query. In that case, go ahead with the closest relevant subreddit.
+* To know that you have reached a subreddit successfully, you will see '/f/subreddit_name' in the observation. 
+* Once you have navigated to any specific subreddit, return stop [N/A]. Even if the subreddit is generally related and not specific to your quwey, stop here and do not try to search again for another subreddit.
 """,
 
 "input": """
@@ -618,7 +624,7 @@ Example actions:
 click [7]
 type [7] [issue] [1]
 stop [N/A]
-goto [http://ec2-3-131-244-37.us-east-2.compute.amazonaws.com:9999/user/]
+goto [https://webarena-env-reddit.awsdev.asapp.com/user/]
 
 You will be provided with the following,
 OBJECTIVE:
@@ -638,8 +644,8 @@ Your action
 
 Please follow these instructions to solve the subtask:
 1. The objective find_user [user_name] asks you to navigate the page of a user with user_name
-2. To do so, look at the current base URL (e.g. http://ec2-3-131-244-37.us-east-2.compute.amazonaws.com:9999) and add a suffix /user/user_name, i.e.
-goto [http://ec2-3-131-244-37.us-east-2.compute.amazonaws.com:9999/user/user_name]
+2. To do so, look at the current base URL (e.g. https://webarena-env-reddit.awsdev.asapp.com) and add a suffix /user/user_name, i.e.
+goto [https://webarena-env-reddit.awsdev.asapp.com/user/user_name]
 3. Once you have navigated to the user page (as seen in your past actions), return stop [N/A]
 """,
 
@@ -973,7 +979,7 @@ You need to generate a response in the following format. Please issue only a sin
 	Your action
 
 Please follow these general instructions:
-1. First check thhe OBJECTIVE. If the OBJECTIVE is a question about my orders, you MUST use search_order [question] to answer the question. For example, 
+1. First check the OBJECTIVE. If the OBJECTIVE is a question about my orders, you MUST use search_order [question] to answer the question. For example, 
 a. search_order [How much I spend on ...?]
 b. search_order [What is the size of the picture frame I bought Sep 2022?]
 c. search_order [Change the delivery address for my most recent order]
@@ -985,9 +991,10 @@ means that the response was $0. In that case, return the answer directly, e.g. s
 If the response was N/A, reply stop [N/A]
 3.  If the OBJECTIVE is a question about listing / showing products, you MUST use list_products. For example,
 a. list_products [List products from PS4 accessories category by ascending price]
+b.  list_products [Show me the most expensive product from skin care tool category]
 4. If the OBJECTIVE requires you to retrieve details about a particular order you placed liked SKU, you MUST first use search_order [] to retrieve the SKU
 a. If the OBJECTIVE is "Fill the "contact us" form in the site for a refund on the bluetooth speaker I bought ... Also, ensure to include the order number #161 and the product SKU."
-you must first issue search_order [Give me the SKU of bluetooth speaker from order number #161]
+you must first issue search_order [Give me the SKU of bluetooth speaker from order number #161]. When filling out the form, avoid qualifiers like "just".
 b. If the OBJECTIVE is "Draft a refund message via their "contact us" form for the phone screen protector I bought March 2023. It broke after three days of use. The shop requires the order id, the reason and the amount to refund in the message."
 you must first issue search_order [Give me the order id and the amount for the phone screen protector I bought March 2023.]
 5. To find the Contact Us link, scroll down in any page till you see it.
@@ -1051,24 +1058,21 @@ ACTION:
 Your action
 
 Please follow these instructions to solve the subtask:
-1. Navigate to My Account, then My Orders to access all orders
-2. The orders are sorted by descending date. Use Page Next to navigate to orders placed at an earlier date than displayed. Use Page Previous to navigate to orders at a later date than displayed.
-click [id] corresponding to Page Previous to go to a later date
-click [id] corresponding to Page Next to go to a earlier date
-3. If you don't see an order for a date, and the first order on the page is after the date, the last order on the page is before the date, then it means there is no order for the date. No point navigating to previous or next pages. 
-4. If the question is how much did I spend on a date, and I didn't spend anything, return stop [$0]
-5. If the status of an order shows cancelled, that means I did not spend that money
-6. If you have to find the total amount you spent on orders that span multiple pages, use note [Spent $10 on 4/1/2024] to make a personal note before moving on to the next page. When you are done, you can look at PREVIOUS ACTIONS to find all notes. 
-7. When you are adding numbers, work out each addition step by step in REASON. 
-8. Use go_back to go back to a previous page from an order. 
-But before you do, use note [] to make a note that you checked the page, e.g. 
-note [Checked order on 11/29/2023, no picture frame.]
-9. If you are asked to change the delivery address on an order, you can't. Reply stop [N/A]
-10. If you are in an order page and need to go back, issue go_back. 
-Don't click on My Orders else you have to start from all over again.
-11. Do not keep visiting the same order page over and over again!
+* Navigate to My Account, then My Orders to access all orders.
+* The orders are sorted by descending date. Click on Page Next to go to a earlier date. Click on Page Previous to go to a earlier date.
+If you don't see an order for a date, and the first order on the page is after the date, the last order on the page is before the date, then it means there is no order for the date.
+* In your REASON, state what is the current range, what range you are looking for, and whether you should search for an earlier or a later date.
+* If you have to find the total amount you spent on orders that span multiple pages, use note [Spent $10 on 4/1/2024] to make a personal note before moving on to the next page. When you are done, you can look at PREVIOUS ACTIONS to find all notes. 
+* When you are adding numbers, work out each addition step by step in REASON. 
+* Use go_back to go back to a previous page from an order. But before you do, use note [] to make a note that you checked the page, e.g. note [Checked order on 11/29/2023, no picture frame.]
+* If you are in an order page and need to go back, issue go_back. Don't click on My Orders else you have to start from all over again.
+* Do not keep visiting the same order page over and over again!
 To prevent this, whenever you visit a page, always make a note. For example note [Nothing relevant purchased on September 29, 2022]
 See note [] to see what dates you have visit, and be sure to not visit that page again.
+* Once you are done visiting all the pages, return stop [answer] with the answer to the query. 
+* If the question is how much did I spend on a date, and I didn't spend anything, return stop [$0]
+* If the status of an order shows cancelled, that means I did not spend that money
+* If you are asked to change the delivery address on an order, you can't. Reply stop [N/A]
 """,
 
 "input": """
@@ -1098,6 +1102,7 @@ Page Operation Actions:
 `type [id] [content] [press_enter_after=0|1]`: Use this to type the content into the field with id. By default, the "Enter" key is pressed after typing unless press_enter_after is set to 0.
 `hover [id]`: Hover over an element with id. Use this whenever any element has the field hasPopup: menu
 `goto [url]`: Navigate to a specific URL. Use this when needing to sort by price. Refer to instructions below.
+`note [content]`: Use this to make a personal note of some content you would like to remember. This shows up in your history of previous actions so you can refer to it. 
 
 Completion Action:
 `stop [answer]`: Issue this action when you believe the task is complete. If the objective is to find a text-based answer, provide the answer in the bracket. If you believe the task is impossible to complete, provide the answer as "N/A" in the bracket.
@@ -1127,7 +1132,8 @@ Your action
 
 Please follow these instructions to solve the subtask:
 1. To find a product category, you MUST use hover [id] to expand the popup Menu till you find the leaf element that has no popup. Then click [id] on the leaf element.
-For exmaple, to find PS 4 accessories you must hover over Video Games, then hover over Playstation 4, then click on Accessories. 
+For exmaple, to find PS 4 accessories you must hover over Video Games, then hover over Playstation 4, then click on Accessories.
+Use note [] eveytime you hover on an item, and don't find the category. This is to ensure you don't keep trying the same category repeatedly.
 2. To sort current list of products by price and in ascending order, you MUST use the goto [url] action by appending ?product_list_order=price to the current URL. For example:
 If URL is https://webarena-env-shopping.awsdev.asapp.com/video-games/playstation-4/accessories.html
 then issue goto [https://webarena-env-shopping.awsdev.asapp.com/video-games/playstation-4/accessories.html?product_list_order=price]
@@ -1138,6 +1144,7 @@ goto [https://webarena-env-shopping.awsdev.asapp.com/video-games/playstation-4/a
 If URL is https://webarena-env-shopping.awsdev.asapp.com/video-games/playstation-4/accessories.html
 goto [https://webarena-env-shopping.awsdev.asapp.com/video-games/playstation-4/accessories.html?price=0-25]
 5. Once you are done in stop [N/A]
+6. If the OBJECTIVE asks you to show the most expensive product, you must click on the product.
 """,
 
 "input": """
@@ -1195,9 +1202,15 @@ ACTION:
 Your action
 
 Please follow these instructions to solve the subtask:
-1. To find the list of reviews, search for a link with Reviewers. If you can't find it, scroll [down].
-2. Not all the reviews are listed on a page. You MUST scroll down till you reach the end of the page. This is the case when you see the footer with Contact us is visible.
-3. If you have to list multiple reviewers, use note [Reviewer X made comment Y] to make a personal note before scrolling down to see other reviews. When you are done, you can look at PREVIOUS ACTIONS to find all notes. In stop [], make sure you answer the question in the OBJECTIVE.
+* If you are not in the product page, you can search for the product using the search bar.
+* To find the list of reviews, search for a link with Reviewers. If you can't find it, scroll down to search for it.
+* Not all reviews will be visible on the reviews page. You MUST scroll down till you reach the end of the page. 
+* You will know that you have reached the end of the page if you see “Contact Us” in the OBSERVATION.
+* As you scroll through the reviews, record each relevant review using the action, note [review]. Important: Only note the sentences in the review that are relevant to the OBJECTIVE. Copy the rest of the sentences VERABITM, separating each sentence by a delimeter.
+* In your REASONS, justify which sentences you selected to note as being relevant to the OBJECTIVE.
+* After scrolling through all reviews, look at your PREVIOUS ACTIONS to see the list of notes you have collected
+* Combine the list of all notes into a concatenated text all_reviews and return these using the action stop [all_reviews].
+* Important: When returning stop [all_reviews], make sure every word in all_reviews is gramatically correct. Words that are joined together should be separate. Drop any sentence in the review that is not relevant to the OBJECTIVE. 
 """,
 
 "input": """
